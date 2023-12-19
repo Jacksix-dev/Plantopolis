9cc2c24936e899bd734c9d4991775640f042c24b
Facundo Gasco <facundogasco@gmail.com>
2023-12-19 17:52:47 -0300

    CRUD added

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,25 +1,47 @@
 class PlantsController < ApplicationController
+before_action :set_plant, only:[:show, :edit , :update, :destroy]
   def index
     @plants = Plant.all
   end
 
   def new
-
+    @plant = Plant.new
   end
 
   def show
-    @plants
+
   end
 
   def update
+    @plant.update!(plant_params)
+    redirect_to @plant
   end
 
   def create
+    @plant = Plant.new(plant_params)
+    @plant.save
+    if @plant.save
+      redirect_to plants_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @plant.destroy
+    redirect_to plants_path
   end
 
   def edit
+
+  end
+
+  private
+
+  def plant_params
+    params.require(:plant).permit(:name, :description, :price, :user_id)
+  end
+  def set_plant
+    @plant = Plant.find(params[:id])
   end
 end
